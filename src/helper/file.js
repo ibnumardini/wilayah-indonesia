1ca4239d22e427da98fce68ed21fb53f48eab6a8
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2025-07-31 21:43:51 +0700

    fix: ensure directory exists before writing to file in writeToFile function

--- src/helper/file.js
+++ src/helper/file.js
@@ -1,11 +1,20 @@
 const fs = require("fs");
 
 exports.writeToFile = (filename, data) => {
-  fs.writeFile(filename, JSON.stringify(data), (err) => {
+  const dirPath = filename.split("/").slice(0, -1).join("/");
+
+  fs.mkdir(dirPath, { recursive: true }, (err) => {
     if (err) {
-      console.error(`Error writing to file ${filename}:`, err);
+      console.error("Error creating folder:", err);
+      return;
     } else {
-      console.log(`Data successfully written to ${filename}`);
+      fs.writeFile(filename, JSON.stringify(data), (err) => {
+        if (err) {
+          console.error(`Error writing to file ${filename}:`, err);
+        } else {
+          console.log(`Data successfully written to ${filename}`);
+        }
+      });
     }
   });
 };
